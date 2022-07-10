0c7a0969f2daace818097574578ed8efa43a04b8
Raymond Chang <rchang0226@gmail.com>
2022-07-09 21:44:03 -0400

    Add slider for grid resolution

--- app.js
+++ app.js
@@ -1,14 +1,36 @@
 const container = document.querySelector('.container');
-for (i = 0; i < 16; i++) {
-    const column = document.createElement('div');
-    column.classList.add("column")
-    container.appendChild(column);
-    for (j = 0; j < 16; j++) {
-        const grid = document.createElement('div');
-        grid.classList.add("grid")
-        column.appendChild(grid);
+
+function populate(size) {
+
+    while (container.firstChild) {
+        container.removeChild(container.lastChild);
     }
+
+    for (i = 0; i < size; i++) {
+        const column = document.createElement('div');
+        column.classList.add("column")
+        container.appendChild(column);
+        for (j = 0; j < size; j++) {
+            const grid = document.createElement('div');
+            grid.classList.add("grid")
+            column.appendChild(grid);
+        }
+    }
+
+    const grids = document.querySelectorAll('.grid');
+    grids.forEach(grid => grid.addEventListener('mouseover', hover));
+    grids.forEach(grid => grid.addEventListener('mouseout', stopHover));
+    grids.forEach(grid => grid.addEventListener('mousedown', draw));
+    grids.forEach(grid => grid.addEventListener('mouseover', draw));
 }
+
+const slider = document.querySelector('.slider');
+const output = document.querySelector('.size');
+output.textContent = `${slider.value} x ${slider.value}`;
+slider.addEventListener('input', () => {
+    populate(slider.value);
+    output.textContent = `${slider.value} x ${slider.value}`;
+});
 
 function hover(e) {
     this.classList.add('hovering');
@@ -27,8 +49,4 @@ let mouseDown = false;
 document.body.onmousedown = () => (mouseDown = true)
 document.body.onmouseup = () => (mouseDown = false)
 
-const grids = document.querySelectorAll('.grid');
-grids.forEach(grid => grid.addEventListener('mouseover', hover));
-grids.forEach(grid => grid.addEventListener('mouseout', stopHover));
-grids.forEach(grid => grid.addEventListener('mousedown', draw));
-grids.forEach(grid => grid.addEventListener('mouseover', draw));
+populate(16);
